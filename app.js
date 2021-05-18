763abec5283553edefa71453d05f42e41924c9c2
Margo <margo@meowmeow>
2021-05-18 13:26:32 +0700

    edit and delete, fixed data

--- app.js
+++ app.js
@@ -6,63 +6,73 @@ app.use(express.static('public'));
 app.use(express.urlencoded({ extended: false }));
 
 const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'kitty',
-    password: 'meow',
-    database: 'nyamkaAPP'
+  host: 'localhost',
+  user: 'kitty',
+  password: 'meow',
+  database: 'nyamkaAPP'
 });
 
 app.get('/', (req, res) => {
-    res.render('top.ejs')
+  res.render('top.ejs')
 });
 
 app.get('/index', (req, res) => {
-    connection.query(
-        'SELECT * FROM items',
-        (error, results) => {
-            console.log("Result: ", results);
-            res.render('index.ejs', { items: results });
+  connection.query(
+    'SELECT * FROM items',
+    (error, results) => {
+      results.forEach(item => {
+        if (item.date !== null) {
+          item.date = item.date.toISOString().split('T')[0]
         }
-    );
+      });
+      console.log("Result: ", results);
+      res.render('index.ejs', { items: results });
+    }
+  );
 });
 
 app.get('/new', (req, res) => {
-    res.render('new.ejs');
+  res.render('new.ejs');
 });
 
 app.post('/create', (req, res) => {
-    connection.query(
-      'INSERT INTO items (items) VALUES (?)',
-      [req.body.itemName],
-      (error, results) => {
-        res.redirect('/index'); 
-      }
-    );
-  });
+  connection.query(
+    'INSERT INTO items (items, price, date) VALUES (?,?,?)',
+    [req.body.itemName, req.body.itemPrice, req.body.date],
+    (error, results) => {
+      res.redirect('/index');
+    }
+  );
+});
 
-  app.post('/delete/:id', (req, res) => {
-    connection.query(
-      'DELETE FROM items WHERE id = ?',
-      [req.params.id],
-      (error, results) => {
-        res.redirect('/index');
-      }
-    );
-  });
+app.post('/delete/:id', (req, res) => {
+  connection.query(
+    'DELETE FROM items WHERE id = ?',
+    [req.params.id],
+    (error, results) => {
+      res.redirect('/index');
+    }
+  );
+});
 
-// app.post('/create_price', (req, res) => {
-//     connection.query(
-//         'INSERT INTO items (price) VALUES (?)',
-//         [req.body.itemPrice],
-//         (error, results) => {
-//             connection.query(
-//                 'SELECT * FROM items',
-//                 (error, results) => {
-//                     res.redirect('/index'); 
-//                 }
-//             );
-//         }
-//     );
-// });
+app.get('/edit/:id', (req, res) => {
+  connection.query(
+    'SELECT * FROM items WHERE id = ?',
+    [req.params.id],
+    (error, results) => {
+      res.render('edit.ejs', { item: results[0] });
+    }
+  );
+});
+
+app.post('/update/:id', (req, res) => {
+  connection.query(
+    'UPDATE items SET name = ? WHERE id = ?',
+    [req.body.itemName, req.params.id],
+    (error, results) => {
+      res.redirect('/index');
+    }
+  );
+});
 
 app.listen(3000);
